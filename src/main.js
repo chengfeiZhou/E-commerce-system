eda0320e9ca511c48cbb1b566eb0923c62b41be8
zhouChengfei <zhouchengfei@megvii.com>
2020-06-15 15:34:29 +0800

    侧边栏布局;fake菜单数据

--- src/main.js
+++ src/main.js
@@ -2,6 +2,17 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import './plugins/element.js'
+import axios from 'axios'
+
+// 设置axios请求拦截器
+axios.defaults.base = 'http://127.0.0.1:8888/api'
+axios.interceptors.request.use(config => {
+  // 加入token
+  config.headers.Authorization = window.sessionStorage.getItem('token')
+  console.log(config)
+  return config
+})
+Vue.prototype.$http = axios
 
 Vue.config.productionTip = false
 
