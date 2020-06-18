d0961273f475201b3bd84c25bb1959cac28c9399
zhouchengfei <t-zhouchenfgei@megvii.com>
2020-06-19 00:04:41 +0800

    路由守护

--- src/router/index.js
+++ src/router/index.js
@@ -6,13 +6,20 @@ import Home from '../components/Home'
 Vue.use(VueRouter)
 
 const routes = [
-  { path: '/', redirect: '/login' }, // 路由重定向
+  { path: '/', redirect: '/home' }, // 路由重定向
   { path: '/login', component: Login },
   { path: '/home', component: Home }
 ]
 
 const router = new VueRouter({
   routes
+})
+router.beforeEach((to, from, next) => {
+  // 如果用户访登录页, 直接放行
+  if (to.path === '/login') return next()
+  // 从sessionStorage中获取保存的token值
+  if (!window.sessionStorage.getItem('token')) return next('/login')
+  next()
 })
 
 export default router
