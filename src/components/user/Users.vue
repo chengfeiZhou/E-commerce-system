d91ae8e65a7ed02825e1f4922e6d463fe64a21da
zhouchengfei <t-zhouchenfgei@megvii.com>
2020-06-24 00:01:13 +0800

    用户列表页展示

--- src/components/user/Users.vue
+++ src/components/user/Users.vue
@@ -1,11 +1,114 @@
 <template>
   <div>
-    <h3>用户列表</h3>
+    <!-- 面包屑导航 -->
+    <el-breadcrumb separator-class="el-icon-arrow-right">
+      <el-breadcrumb-item :to="{ path: '/home' }">首页</el-breadcrumb-item>
+      <el-breadcrumb-item>用户管理</el-breadcrumb-item>
+      <el-breadcrumb-item>用户列表</el-breadcrumb-item>
+    </el-breadcrumb>
+    <!-- 卡片 -->
+    <el-card>
+      <el-row :gutter="20">
+        <el-col :span="7">
+          <el-input placeholder="请输入内容">
+          <el-button slot="append" icon="el-icon-search"></el-button>
+        </el-input>
+        </el-col>
+        <el-col :span="4">
+          <el-button type="primary">添加用户</el-button>
+        </el-col>
+      </el-row>
+      <el-table
+        :border="true"
+        :stripe="true"
+        :data="userList"
+        style="width: 100%">
+        <el-table-column type="index" width="50"></el-table-column>
+        <el-table-column prop="username" label="姓名"></el-table-column>
+        <el-table-column prop="email" label="邮件"></el-table-column>
+        <el-table-column prop="mobile" label="电话"></el-table-column>
+        <el-table-column prop="role_name" label="角色"></el-table-column>
+        <el-table-column label="状态">
+          <!-- 作用域插槽 -->
+          <template slot-scope="scope">
+            <el-switch v-model="scope.row.mg_state"></el-switch>
+          </template>
+        </el-table-column>
+        <el-table-column label="操作" width="180px">
+          <template>
+            <el-tooltip effect="dark" content="编辑用户" placement="top" :enterable="false">
+              <el-button type="primary" icon="el-icon-edit" size="mini"></el-button>
+            </el-tooltip>
+            <el-tooltip effect="dark" content="删除用户" placement="top" :enterable="false">
+              <el-button type="danger" icon="el-icon-delete" size="mini"></el-button>
+            </el-tooltip>
+            <el-tooltip effect="dark" content="分配角色" placement="top" :enterable="false">
+              <el-button type="warning" icon="el-icon-setting" size="mini"></el-button>
+            </el-tooltip>
+          </template>
+        </el-table-column>
+      </el-table>
+    </el-card>
   </div>
 </template>
 
 <script>
 export default {
+  data () {
+    return {
+      queryInfo: {
+        query: '',
+        pagenum: 1,
+        pagesize: 20
+      },
+      userList: [],
+      total: 0
+    }
+  },
+  created () {
+    this.getUserList()
+  },
+  methods: {
+    async getUserList () {
+      // const {data: res} = await this.$http.get('users', { parms: this.queryInfo })
+      const res = {
+        data: {
+          totalpage: 5,
+          pagenum: 4,
+          total: 20,
+          users: [
+            {
+              id: 25,
+              username: 'tige117',
+              mobile: '18616358651',
+              type: 1,
+              email: 'tige112@163.com',
+              create_time: '2017-11-09T20:36:26.000Z',
+              mg_state: true, // 当前用户的状态
+              role_name: '炒鸡管理员'
+            },
+            {
+              id: 26,
+              username: '的深层次的',
+              mobile: '18616358651',
+              type: 1,
+              email: 'tige112@163.com',
+              create_time: '2017-11-09T20:36:26.000Z',
+              mg_state: false, // 当前用户的状态
+              role_name: '管理员'
+            }
+          ]
+        },
+        meta: {
+          msg: '获取成功',
+          status: 200
+        }
+      }
+      if (res.meta.status !== 200) return this.$message.error('获取用户失败')
+      this.userList = res.data.users
+      this.total = res.data.total
+    }
+  }
 }
 </script>
 
--- src/plugins/element.js
+++ src/plugins/element.js
@@ -11,7 +11,16 @@ import {
   Main,
   Menu,
   Submenu,
-  MenuItem
+  MenuItem,
+  Breadcrumb,
+  BreadcrumbItem,
+  Card,
+  Row,
+  Col,
+  Table,
+  TableColumn,
+  Switch,
+  Tooltip
 } from 'element-ui'
 Vue.use(Button)
 Vue.use(Form)
@@ -24,5 +33,14 @@ Vue.use(Main)
 Vue.use(Menu)
 Vue.use(Submenu)
 Vue.use(MenuItem)
+Vue.use(Breadcrumb)
+Vue.use(BreadcrumbItem)
+Vue.use(Card)
+Vue.use(Row)
+Vue.use(Col)
+Vue.use(Table)
+Vue.use(TableColumn)
+Vue.use(Switch)
+Vue.use(Tooltip)
 // 全局挂载
 Vue.prototype.$message = Message
