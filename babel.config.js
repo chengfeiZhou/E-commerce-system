4dcfcfa2b53152ff0853a1a9788d174d27329019
zhou_chengfei <ZZZ1171284619@gmail.com>
2020-07-05 21:57:02 +0800

    Revert "Packweb"

--- babel.config.js
+++ babel.config.js
@@ -1,11 +1,7 @@
-// 项目发布阶段需要用到的babel插件
-const prodPlugins = []
-if (process.env.NODE_ENV === 'production') {
-  prodPlugins.push('transform-remove-console')
-}
 module.exports = {
-  presets: ['@vue/app'],
-
+  presets: [
+    '@vue/cli-plugin-babel/preset'
+  ],
   plugins: [
     [
       'component',
@@ -13,10 +9,6 @@ module.exports = {
         libraryName: 'element-ui',
         styleLibraryName: 'theme-chalk'
       }
-    ],
-    // 发布产品时的插件数组
-    ...prodPlugins,
-    //配置路由懒加载插件
-    "@babel/plugin-syntax-dynamic-import"
+    ]
   ]
 }
--- src/main.js
+++ src/main.js
@@ -1,30 +1,19 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
-// import './plugins/element.js'
+import './plugins/element.js'
 import axios from 'axios'
 
 // 导入全局样式表
 import './assets/css/global.css'
 // 导入字体图标
 import './assets/fonts/iconfont.css'
-
-// 导入进度条包
-import NProgress from 'nprogress'
-// import 'nprogress/nprogress.css'
-
 // 设置axios请求拦截器
 axios.defaults.base = 'http://127.0.0.1:8888/api'
 axios.interceptors.request.use(config => {
-  // 展示进度条
-  NProgress.start()
   // 加入token
   config.headers.Authorization = window.sessionStorage.getItem('token')
-  return config
-})
-axios.interceptors.response.use(config => {
-  NProgress.done()
-  NProgress.remove()
+  console.log(config)
   return config
 })
 Vue.prototype.$http = axios
